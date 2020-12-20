667b386ed8689e419c7de6bb03bfb4ebbe28ddc0
luoyincheng <yincheng.luo@gmail.com>
2020-12-20 16:27:55 +0800

    common

--- 2_hello_triangle/src.cpp
+++ 2_hello_triangle/src.cpp
@@ -1,14 +1,11 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "../common/configs.h"
 
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
-
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
