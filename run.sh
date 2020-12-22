2847c012620b4852d147bf65a67b8181cae2a29a
luoyincheng <yincheng.luo@gmail.com>
2020-12-22 22:18:37 +0800

    rearrange

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 root_path=`pwd`
-glad_path=${root_path}/glad.c
+glad_path=${root_path}/common/glad.c
+app_dir=${root_path}/app
 cat << EOF
 
 (1) Hello Window
@@ -13,25 +14,22 @@ read -p "select a demo from above to run to: " input
 case $input in 
 
 1) source_path=${root_path}/1_hello_window/src.cpp
-   app_dir=${root_path}/1_hello_window
-   g++ -o ${app_dir}/app ${source_path} ${glad_path} -lglfw3 -lm -lXrandr -lXi -lX11 -lpthread -ldl -lXinerama -lXcursor
-   cd ${app_dir}
+   g++ -o ${root_path}/app ${source_path} ${glad_path} -lglfw3 -lm -lXrandr -lXi -lX11 -lpthread -ldl -lXinerama -lXcursor
+   cd ${root_path}
    ./app
-   rm ${app_dir}/app;;
+   rm ${root_path}/app;;
 
 2) source_path=${root_path}/2_hello_triangle/src.cpp
-   app_dir=${root_path}/2_hello_triangle
-   g++ -o ${app_dir}/app ${source_path} ${glad_path} -lglfw3 -lm -lXrandr -lXi -lX11 -lpthread -ldl -lXinerama -lXcursor
-   cd ${app_dir}
+   g++ -o ${root_path}/app ${source_path} ${glad_path} -lglfw3 -lm -lXrandr -lXi -lX11 -lpthread -ldl -lXinerama -lXcursor
+   cd ${root_path}
    ./app
-   rm ${app_dir}/app;;
+   rm ${root_path}/app;;
 
 3) source_path=${root_path}/3_shaders/src.cpp
-   app_dir=${root_path}/3_shaders
-   g++ -o ${app_dir}/app ${source_path} ${glad_path} -lglfw3 -lm -lXrandr -lXi -lX11 -lpthread -ldl -lXinerama -lXcursor
-   cd ${app_dir}
+   g++ -o ${root_path}/app ${source_path} ${glad_path} -lglfw3 -lm -lXrandr -lXi -lX11 -lpthread -ldl -lXinerama -lXcursor
+   cd ${root_path}
    ./app
-   rm ${app_dir}/app;;
+   rm ${root_path}/app;;
 esac
 
 # g++ -o ~/Desktop/projects/OpenGLBP/out ~/Desktop/projects/OpenGLBP/gears.c ~/Desktop/projects/OpenGLBP/glad.c -lglfw3 -lm -lXrandr -lXi -lX11 -lpthread -ldl -lXinerama -lXcursor
